80222b467265c9bf199885dcd39f7c2c8b877b07
Denis Lunev <den.lunev@gmail.com>
2016-02-05 10:41:20 -0600

    Added raw  error message

--- src/main/java/com/integrationagent/hubspotApi/utils/HubSpotException.java
+++ src/main/java/com/integrationagent/hubspotApi/utils/HubSpotException.java
@@ -8,6 +8,8 @@ public class HubSpotException extends Exception {
 
 	private int code;
 
+	private String rawMessage;
+
 	public HubSpotException(String message) {
 		super(message);
 	}
@@ -21,7 +23,20 @@ public class HubSpotException extends Exception {
 		this.code = code;
 	}
 
+	public HubSpotException(String message, String rawMessage) {
+		super(message);
+		this.rawMessage = rawMessage;
+	}
+
 	public int getCode() {
 		return code;
+	}
+
+	public String getRawMessage() {
+		return rawMessage;
+	}
+
+	public void setRawMessage(String rawMessage) {
+		this.rawMessage = rawMessage;
 	}
 }
