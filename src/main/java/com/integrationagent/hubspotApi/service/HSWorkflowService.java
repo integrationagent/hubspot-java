0245673a283fa321a688f9ec87e2758d69d92ed0
Denis Lunev <den.lunev@gmail.com>
2016-06-21 17:22:32 -0500

    Added enroll workflow

--- src/main/java/com/integrationagent/hubspotApi/service/HSWorkflowService.java
+++ src/main/java/com/integrationagent/hubspotApi/service/HSWorkflowService.java
@@ -1,5 +1,7 @@
 package com.integrationagent.hubspotApi.service;
 
+import com.integrationagent.hubspotApi.utils.HubSpotException;
+
 public class HSWorkflowService {
 
     private HttpService httpService;
@@ -7,4 +9,13 @@ public class HSWorkflowService {
     public HSWorkflowService(HttpService httpService) {
         this.httpService = httpService;
     }
+
+    public void enroll(String workflowId, String email) throws HubSpotException {
+
+		String url = "https://api.hubapi.com/automation/v2/workflows/" + workflowId + "/enrollments/contacts/" + email;
+
+		httpService.postRequest(url, "");
+	}
+
+
 }
