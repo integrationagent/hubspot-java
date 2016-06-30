a5a9edc710aa2d17267b10be17cda0f0824eaacb
Denis Lunev <den.lunev@gmail.com>
2016-06-30 14:26:12 -0500

    Engagement object fix

--- src/main/java/com/integrationagent/hubspotApi/service/HSEngagementService.java
+++ src/main/java/com/integrationagent/hubspotApi/service/HSEngagementService.java
@@ -16,14 +16,11 @@ public class HSEngagementService {
      public Long create(Long contactId, String body) throws HubSpotException {
         HSEngagement engagement = new HSEngagement(HSEngagement.Type.NOTE, contactId, body);
         String url = "/engagements/v1/engagements";
-<<<<<<< HEAD
-        JsonNode jsonNode = httpService.postRequest(url, engagement.toString(), null);
 
-        return jsonNode.getObject().getJSONObject("engagement").getLong("id");
-=======
-        JSONObject jsonObject = (JSONObject) httpService.postRequest(url, engagement.toString());
-        return jsonObject.getJSONObject("HSEngagement").getLong("id");
->>>>>>> 4a410bcf30cb24e662abb7c37df903b4b44ac7f6
+        JSONObject jsonNode = (JSONObject) httpService.postRequest(url, engagement.toString());
+
+        return jsonNode.getJSONObject("engagement").getLong("id");
+
     }
 
     public void delete(Long engagementId) throws UnirestException, HubSpotException {
