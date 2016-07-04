5b16a045d13cf2d087d055fa914b24327b255af3
Sergei Dubinin <sdubininit@gmail.com>
2016-07-04 19:54:03 +0300

    integration tests

--- src/test/java/com/integrationagent/hubspotApi/integraion/HSFormServiceIT.java
+++ src/test/java/com/integrationagent/hubspotApi/integraion/HSFormServiceIT.java
@@ -1,4 +1,32 @@
 package com.integrationagent.hubspotApi.integraion;
 
+import com.integrationagent.hubspotApi.domain.HSForm;
+import com.integrationagent.hubspotApi.service.HubSpot;
+import com.integrationagent.hubspotApi.utils.Helper;
+import com.integrationagent.hubspotApi.utils.UrlMap;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
+
 public class HSFormServiceIT {
+
+    private HubSpot hubSpot = new HubSpot(Helper.getProperty("hubspot.apikey"));
+
+    @Test
+    public void getAll_Test() throws Exception {
+        List<HSForm> forms = hubSpot.form().getAll();
+        assertNotNull(forms);
+    }
+
+    @Test
+    @Ignore
+    public void submit_Test() throws Exception {
+        String formId = "form ID";
+        UrlMap properties = new UrlMap();
+        String PortalId = "portal ID";
+        hubSpot.form().submit(formId, properties, PortalId);
+    }
 }
--- src/test/java/com/integrationagent/hubspotApi/integraion/HSWorkflowServiceIT.java
+++ src/test/java/com/integrationagent/hubspotApi/integraion/HSWorkflowServiceIT.java
@@ -1,4 +1,19 @@
 package com.integrationagent.hubspotApi.integraion;
 
+import com.integrationagent.hubspotApi.service.HubSpot;
+import com.integrationagent.hubspotApi.utils.Helper;
+import org.junit.Ignore;
+import org.junit.Test;
+
 public class HSWorkflowServiceIT {
+
+    private HubSpot hubSpot = new HubSpot(Helper.getProperty("hubspot.apikey"));
+
+    @Test
+    @Ignore
+    public void enroll_Test() throws Exception {
+        String workflowId = "workflow ID";
+        String email = "email";
+        hubSpot.workflow().enroll(workflowId, email);
+    }
 }
